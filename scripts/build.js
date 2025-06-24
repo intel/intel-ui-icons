baef34839e042cf781c7d6e3e84468eb40ba12cf
Francesa Alfaro, Agustin <agustin.francesa.alfaro@intel.com>
2025-06-24 08:50:17 -0600

    ci: publishing package to npm registry
    Signed-off-by: Francesa Alfaro, Agustin <agustin.francesa.alfaro@intel.com>

--- scripts/build.js
+++ scripts/build.js
@@ -1,22 +1,26 @@
 const fs = require('fs-extra')
 const path = require('path')
 const glob = require('glob')
+const distFolder = 'dist.web'
 
-// Create dist.web directory if it doesn't exist
-fs.ensureDirSync('dist.web')
-
-// Copy LICENSE.md file if it exists
-if (fs.existsSync('LICENSE.md')) {
-    console.log('Copying LICENSE.md file...')
-    fs.copySync('LICENSE.md', 'dist.web/LICENSE.md')
-    console.log('Copied: LICENSE.md -> dist.web/LICENSE.md')
+//Delete and create the dist directory
+if (fs.existsSync(distFolder)) {
+    console.log('Removing existing dist directory...')
+    fs.removeSync(distFolder)
 }
+console.log('Creating dist directory...')
+fs.ensureDirSync(distFolder)
 
-// Copy README.md file if it exists
-if (fs.existsSync('README.md')) {
-    console.log('Copying README.md file...')
-    fs.copySync('README.md', 'dist.web/README.md')
-    console.log('Copied: README.md -> dist.web/README.md')
-}
+const elementsToCopy = ['LICENSE.md', 'README.md', 'package.json', 'CHANGELOG.md']
+
+elementsToCopy.forEach((element) => {
+    if (fs.existsSync(element)) {
+        console.log(`Copying ${element}...`)
+        fs.copySync(element, path.join(distFolder, element))
+        console.log(`Copied: ${element} -> dist/${element}`)
+    } else {
+        console.warn(`Warning: ${element} does not exist and will not be copied.`)
+    }
+})
 
 console.log('Build completed successfully!')
